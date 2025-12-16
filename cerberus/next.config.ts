b43dfec1080f70ba1edeae07692926eb58067d63
mafagina <matteo.faginali@gmail.com>
2025-12-16 05:53:05 +0100

    Fix: configure Vercel build for subdirectory

--- cerberus/next.config.ts
+++ cerberus/next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.resolve(__dirname),
+  /* config options here */
 };
 
 export default nextConfig;
